1c424e2436afb621a41c4de5ca388457f1ce662e
Leo <hodongnhut@gmail.com>
2025-12-25 09:26:37 +0700

    update

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -18,7 +18,7 @@ AppAsset::register($this);
 <head>
     <meta charset="<?= Yii::$app->charset ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="/favicon.ico">
+    <link rel="icon" href="favicon.ico">
     <?php $this->registerCsrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
